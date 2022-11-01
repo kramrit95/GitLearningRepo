a895f9107270691138347b34ffba17ebf6b14529
team1 <kramrit95@gmail.com>
2022-11-01 07:01:39 +0530

    some change

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log(document.querySelector('#my-form'));
 console.log(document.querySelector('h1'));
 console.log(document.querySelectorAll('.items'));
 alert("warning")
+alert("new change in dev")
